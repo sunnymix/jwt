66c6413d4b138d71db14e40c96176c14ad1d1352
sunny <sunny@sunny.local>
2022-01-11 23:51:33 +0800

    项目初始化

---
+++ src/main/java/Jwt.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Jwt {
+
+    public static void main(String[] args) {
+        List<String> argList = Arrays.asList(args);
+        System.out.println(argList);
+    }
+
+}
