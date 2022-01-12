4a3dd91466ed35635f7cd28cbd5cc2a254ee74df
sunny <sunny@sunnymix.com>
2022-01-12 23:41:51 +0800

    设计参数列表

--- src/main/java/Jwt.java
+++ src/main/java/Jwt.java
@@ -3,6 +3,16 @@ import java.util.List;
 
 public class Jwt {
 
+    /**
+     * 参数列表：
+     * args[0] command
+     * args[1] secret
+     * args[2] claim1=value1
+     * ...
+     * args[N] claimN[=valueN]
+     *
+     * @param args 参数列表
+     */
     public static void main(String[] args) {
         List<String> argList = Arrays.asList(args);
         System.out.println(argList);
