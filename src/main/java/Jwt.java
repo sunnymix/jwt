ccd9a7f265c9c3652d0e8a1ba1d2c399801a6032
sunny <sunny@sunnymix.com>
2022-01-12 23:51:24 +0800

    添加参数列表说明

--- src/main/java/Jwt.java
+++ src/main/java/Jwt.java
@@ -5,11 +5,11 @@ public class Jwt {
 
     /**
      * 参数列表：
-     * args[0] command
-     * args[1] secret
-     * args[2] claim1=value1
+     * args[0] command：encode 编码，decode 解码。
+     * args[1] secret：秘钥。
+     * args[2] claim1=value1：数据项1=数据项1的值。
      * ...
-     * args[N] claimN[=valueN]
+     * args[N] claimN[=valueN]：数据项N=数据项N的值。注意，"=数据项N的值"是可选的，当不传值时会使用默认值。
      *
      * @param args 参数列表
      */
